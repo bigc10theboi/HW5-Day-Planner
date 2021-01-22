98ccf52e130331d573068ffbbf96e3c466473cd0
Carter Venne <you@example.com>
2021-01-21 22:09:58 -0600

    added trim, commented out non functional links

--- Develop/java.js
+++ Develop/java.js
@@ -47,7 +47,7 @@ colors();
 //local storage//
 $(".saveBtn").on("click", function() {
   var time = $(this).parent().attr("id");
-  var schedule = $(this).siblings(".To-do").val();
+  var schedule = $(this).siblings(".To-do").val().trim();
 
   localStorage.getItem(time, schedule);
   localStorage.setItem(time, schedule);
