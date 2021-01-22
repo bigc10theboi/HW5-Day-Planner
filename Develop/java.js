3d1f88c233376c3fd3850c623f7f0e51d6401939
Carter Venne <you@example.com>
2021-01-21 21:12:52 -0600

    got moment to retrieve time

--- Develop/java.js
+++ Develop/java.js
@@ -1,12 +1,26 @@
 //variables//
+var now = moment();
+var dateFormat = "MM/DD/YYYY";
+var convertedDate = moment(now, dateFormat);
+    console.log(convertedDate.format("MM/DD/YY hh:mm"));
+    console.log(convertedDate.format("MM - Do - YY"));
+var newDate = moment();
+    console.log(newDate.diff(convertedDate, "days"));
+
+//function for color coding//
 
 
 //function to display date//
 function displayCurrentDate() {
-    var currentDate = moment().format("dddd, MMMM Do, YYYY");
-    $("#currentDay").text(currentDate);
-  }
-  displayCurrentDate();
-
+  var currentDate = moment().format("dddd, MMMM, YYYY");
+  $("#currentDay").text(currentDate);
+}
+displayCurrentDate();
 
 //local storage//
+$(".saveBtn").on("click", function() {
+    var entry = $(this).siblings(".To-do").val()
+    var time = $(this).siblings().attr("data-time")
+  localStorage.setItem(time, entry);
+  localStorage.getItem(".To-do", "data-time");
+});
